bc8a4864ddfaf5dfae8f8f49fdd9e5d2fd9a0217
Arthur Araujo <aaraujjo@outlook.com.br>
2020-12-09 06:10:09 -0300

    create starship card

---
+++ components/StarshipCard/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Image from "next/image";
+
+import styles from "./StarshipCard.module.css";
+
+//import carbonRocket from "assets/images/carbon_rocket.png";
+
+const StarshipCard = ({ name, mglt, stops }) => {
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.cardInner}>
+        <Image
+          src="/assets/images/carbon_rocket.png"
+          alt={name}
+          width={50}
+          height={50}
+        />
+        <h3>{name}</h3>
+        <h4>MGLT: {mglt}</h4>
+        <h4>STOPS: {stops}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default StarshipCard;
